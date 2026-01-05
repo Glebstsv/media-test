5889f5d6e78ffa283138113f0d6c75d1c6160d37
GDS <glebsstasevich@gmail.com>
2026-01-05 12:21:41 +0300

    ci fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,23 +3,23 @@ import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-    {
-        files: ['**/*.{js,mjs,cjs,jsx}'],
-        plugins: { js },
-        extends: ['js/recommended'],
-        rules: {
-            'no-unused-vars': ['error', { 
-                argsIgnorePattern: '^_',
-                caughtErrors: 'none'
-            }],
-        },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      'no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        caughtErrors: 'none'
+      }],
     },
-    {
-        files: ['**/*.{js,mjs,cjs,jsx}'],
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-            },
-        },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
     },
+  },
 ]);
